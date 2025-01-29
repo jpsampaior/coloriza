a1e9315a36ae13a874338ef6c6f3a3e94c0b8190
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2025-01-29 16:59:55 -0300

    ✨ add link to logo - auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-import { HeroHeader } from "@/components/hero-header/hero-header";
 import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,13 +20,15 @@ export default function RootLayout({
       <body
         className={`relative h-full font-sans antialiased bg-sky-100 ${inter.className}`}
       >
-        <Image
-          src="/logo-coloriza.png"
-          alt="Coloriza"
-          width={64}
-          height={64}
-          className="absolute top-4 left-4 z-50"
-        />
+        <Link href="/">
+          <Image
+            src="/logo-coloriza.png"
+            alt="Coloriza"
+            width={64}
+            height={64}
+            className="absolute top-4 left-4 z-50"
+          />
+        </Link>
         <main className="flex items-center h-full">{children}</main>
       </body>
     </html>
