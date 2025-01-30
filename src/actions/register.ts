93d719c1d36ca464c65d77edfbc339aa3ea37ed3
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2025-01-30 17:11:40 -0300

    ✨ creating user

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { signUpSchema } from "@/lib/validations/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof signUpSchema>) => {
   const validatedFields = signUpSchema.safeParse(values);
@@ -10,5 +13,24 @@ export const register = async (values: z.infer<typeof signUpSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent" };
+  const { email, password, fullName } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name: fullName,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
