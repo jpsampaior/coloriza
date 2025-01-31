95347c2508cc0fc654c4b1299ab9bd30b1ec6495
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2025-01-31 14:43:58 -0300

    ✨ callbacks

--- auth.ts
+++ auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
-
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -11,6 +11,46 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    // TODO: Uncomment the following code to enable email verification
+
+    // async signIn({ user }) {
+    //   if (!user.id) {
+    //     return false;
+    //   }
+
+    //   const existingUser = await getUserById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      // TODO: Add the user's role to the session
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
