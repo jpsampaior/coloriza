c3b3752859a4aa8928b0d7f25f043b5468814a17
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2025-01-31 19:48:23 -0300

    ✨ add role to user

--- actions/register.ts
+++ actions/register.ts
@@ -7,6 +7,7 @@ import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { UserRole } from "@prisma/client";
 
 export const register = async (values: z.infer<typeof signUpSchema>) => {
   const validatedFields = signUpSchema.safeParse(values);
@@ -15,7 +16,7 @@ export const register = async (values: z.infer<typeof signUpSchema>) => {
     return { error: "Campos inválidos!" };
   }
 
-  const { email, password, fullName } = validatedFields.data;
+  const { email, password, fullName, accessCode } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const existingUser = await getUserByEmail(email);
@@ -24,11 +25,25 @@ export const register = async (values: z.infer<typeof signUpSchema>) => {
     return { error: "Email já está em uso!" };
   }
 
+  let role;
+
+  switch (accessCode) {
+    case process.env.ADMIN_ACCESS_CODE:
+      role = UserRole.ADMIN;
+      break;
+    case process.env.USER_ACCESS_CODE:
+      role = UserRole.USER;
+      break;
+    default:
+      return { error: "Código de acesso inválido!" };
+  }
+
   await db.user.create({
     data: {
       name: fullName,
       email,
       password: hashedPassword,
+      role,
     },
   });
 
